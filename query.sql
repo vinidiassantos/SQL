fb3aaf80974e98709f8d5284f88268ab51be0a65
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-14 20:48:58 -0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,26 @@
+SELECT
+-- SQL Server, Postgres, Oracle, mySql
+SELECT coluna1, coluna2
+FROM tabela
+
+SELECT *
+FROM tabela
+
+-- SQL Server, Postgres, Oracle, mySql
+SELECT coluna1, coluna2
+FROM tabela
+
+SELECT *
+FROM tabela
+SELECT *
+FROM person.Person;
+
+SELECT *
+FROM person.person
+WHERE LastName = 'miller'
+
+SELECT *
+FROM person.person
+WHERE LastName = 'miller' and FirstName = 'Anna'
+
+
