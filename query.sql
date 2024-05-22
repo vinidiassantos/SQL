9b8f80a1a999d48107a878bd4bde29fe5303f944
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-22 18:50:53 -0300

    code review

--- query.sql
+++ query.sql
@@ -20,4 +20,17 @@ SELECT *
 FROM person.person
 WHERE LastName = 'miller' and FirstName = 'Anna'
 
+## seleciona todas as colunas da tabela cliente
+SELECT
+    cliente.id,
+    cliente.geografia_id,
+    cliente.descricao,
+    geografia.cidade,
+    geografia.uf,
+    geografia.estado,
+    regioesbrasil.regiao
+FROM cliente c
+LEFT JOIN geografia g ON c.geografia_id = g.id
+LEFT JOIN regioesbrasil r ON regioesbrasil.uf = geografia.uf
+
 
